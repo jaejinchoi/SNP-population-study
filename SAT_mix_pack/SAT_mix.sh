7137428aa600daa2a8ff89b841a47b3cfbc1a717
JaeJin Choi <desert31@hotmail.com>
2023-03-06 12:30:34 -0800

    Update SAT_mix.sh
    cleaup code and replace commands

--- SAT_mix_pack/SAT_mix.sh
+++ SAT_mix_pack/SAT_mix.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
 
-VERSION="2014-7"
+VERSION="2023-3; 2021-2; 2014-7"
 
 # Declare functions
 print_help_and_exit () {
     echo -e "Determine three population analyses based on SNP data with a VCF, a PED, a HapMap, a Simple SNP or a GDS file" 1>&2
     echo -e "From SNPhylo code additional modication was done for PAPGI study by JaeJin Choi, 2014\n"
-    echo -e "Adjusted functions(reqiure installation)"
+    echo -e "Added features (require external programs)"
     echo -e "1. Admixture"
     echo -e "2. TreeMix"
     echo 1>&2
     echo -e "Version: ${VERSION}, customized by JaeJin Choi, KOBIC" 1>&2
     echo 1>&2
     echo -e "Usage:" 1>&2
-    echo -e "\t$(basename $0)
+    echo -e "$(basename $0)
     \n----------Input file type with related arguments
     [-v VCF_file | -p Maximum_PLCS (5) | -c Minimum_depth_of_coverage (5)]
     [-H HapMap_file | -p Maximum_PNSS (0)]
     [-s Simple_SNP_file |-p Maximum_PNSS (0)]
     [-d GDS_file | -l LD_threshold (0.5) | -m MAF_threshold (0.5)]
     [-a PED(ACGT)_file]
+    [-T [int], a number of thread (for sequence alignment)]
 
     \n----------Basic parameters
     [-l [float], LD-linkage disequilibrium]
     [-m [float], MAF-minor allele frequency]
     [-M [float], Missing_rate(0)]
     [-P [absolute_path], prefix_output_folder (default = ./output)]
+    [-T [int], threads when multithreading is available (default = 6)]
     
     \n----------Functional parameters
+    Parameters with '*' at the end are necessary to TURN-ON its function
+    
     1. Admixture [ Population K from -k to -K
-    [-k (int > 1), From]*
-    [-K (int > -k), To]*
+    [-k (int > 1), From] *
+    [-K (int > -k), To] *
 
     2. Treemix
-    [-t (treemix_group_index), see group_index_file format]*
+    [-t (treemix_group_index), see group_index_file format] *
+    [-r [str], Specific an outgroup OTU to reroot TREEMIX tree] 
+    [-R [int], maximum migration]
 
     3. SNPhylo
-    [-b (int >= 100), number of bootstrap_sample, normally use 100]*
-    [-o (str), Outgroup_sample_name for a rooted tree] 
+    [-b (int >= 100), number of bootstrap_sample, normally use 100] *
+    [-o [str], Specific an outgroup OTU to reroot SNPhylo tree] 
     
-    Parameters '*' at the end are necessary to TURN-ON its function
     \n----------ETC
     [-h, help]" 1>&2
-	echo
-    echo 1>&2
-    echo -e "Options:" 1>&2
-    echo -e "\t-b: Performs (non-parametric) bootstrap analysis and generate a tree" 1>&2
-    echo -e "\t-h: Show help and exit" 1>&2
-    echo 1>&2
+    echo
     echo -e "Acronyms:" 1>&2
     echo -e "\tPLCS: The percent of Low Coverage Sample" 1>&2
     echo -e "\tPNSS: The percent of Sample which has no SNP information" 1>&2
@@ -107,6 +107,7 @@ ld_threshold=0.5 #default ld range, 50
 maf_threshold=0.5
 missing_rate=0 #missing rate = 0(none), however, there are 'N'; gap bases in sequence
 prefix_output_folder="${pwd}"
+n_thread=6
 
 admix_from=0
 admix_to=0
@@ -121,7 +122,7 @@ num_bs_sample=0
 admix_from=2
 
 # Parse positional parameters
-while getopts "v:H:d:a:c:p:l:k:K:t:r:R:m:M:P:o:s:b:h" OPT
+while getopts "v:H:d:a:c:p:l:k:K:t:r:R:m:M:P:o:s:b:hT:" OPT
 do
     case "${OPT}" in
         'v')
@@ -157,12 +158,12 @@ do
         'k')
             admix_from="${OPTARG}"
             ;;
-        'k')
+        'K')
             admix_to="${OPTARG}"
             ;;
         't')
             treemix_index_path="${OPTARG}"
-	        ;;
+            ;;
         'r')
             treemix_root="${OPTARG}"
             ;;
@@ -178,6 +179,9 @@ do
         'b')
             num_bs_sample="${OPTARG}"
             ;;
+        'T')
+            n_thread="${OPTARG}"
+            ;;        
         'h')
             print_help_and_exit 0
             ;;
@@ -209,9 +213,9 @@ prefix_output="${prefix_output_folder}/output"
 
 if [ ! -e ${prefix_output_folder} ]
 then
-	mkdir ${prefix_output_folder}
+    mkdir ${prefix_output_folder}
 else
-	rm -f ${prefix_output_folder}/*
+    rm -f ${prefix_output_folder}/*
 fi
 
 
@@ -225,7 +229,11 @@ then
     echo "Start to remove low quality data."
     mnn_filtered_file="${prefix_output}.filtered.vcf"
 
-    "${PYTHON}" "${SCRIPTS_DIR}/remove_low_depth_genotype_data.py" "${vcf_file}" ${min_depth_of_coverage} ${max_plcs_pnss} > "${mnn_filtered_file}"
+    #force feed whole, by copying
+    echo "Force-feed ${vcf_file} to ${mnn_filtered_file}, ignore -c and -p"
+    cp ${vcf_file} ${mnn_filtered_file}
+   
+    #"${PYTHON}" "${SCRIPTS_DIR}/remove_low_depth_genotype_data.py" "${vcf_file}" ${min_depth_of_coverage} ${max_plcs_pnss} > "${mnn_filtered_file}"
     [ $? != 0 ] && exit 1
 
     # Determine and show the number of removed SNP data
@@ -267,7 +275,7 @@ then
     "${PYTHON}" "${SCRIPTS_DIR}/convert_simple_to_hapmap.py" "${smp_file}" > "${prefix_output}.hapmap"
     [ $? != 0 ] && print_help_and_exit 1 "Error: The simple SNP file could not be converted to a HapMap file.\nPlease check the file and restart this script."
 
-    # Remove HapMap data which have many no genotype data
+    # Remove HapMap data which have insufficient genotype data
     echo "Start to remove low quality data."
 
     mnn_filtered_file="${prefix_output}.filtered.hapmap"
@@ -331,8 +339,29 @@ fi
 
 
 # Generate sequences from SNP data, and ${prefix_output}.picked.hapmap 
-"${R}" --slave --vanilla --file="${SCRIPTS_DIR}/generate_snp_sequence.R" --args -H "${mnn_filtered_file}" -l "${ld_threshold}" -m "${maf_threshold}" -M "${missing_rate}" -o "${prefix_output}"
-[ $? != 0 ] && exit 1
+# there are three accepted file formats (vcf, hapmap, and gds), and other formats are converted to one of three before feeded
+if [ ! -z "${vcf_file}" ]
+then
+    "${R}" --slave --vanilla --file="${SCRIPTS_DIR}/generate_snp_sequence.R" --args -v "${mnn_filtered_file}" -l "${ld_threshold}" -m "${maf_threshold}" -M "${missing_rate}" -o "${prefix_output}" > /dev/null
+    [ $? != 0 ] && exit 1
+   
+elif [ ! -z "${hap_file}" ]
+then
+    "${R}" --slave --vanilla --file="${SCRIPTS_DIR}/generate_snp_sequence.R" --args -H "${mnn_filtered_file}" -l "${ld_threshold}" -m "${maf_threshold}" -M "${missing_rate}" -o "${prefix_output}" > /dev/null
+    [ $? != 0 ] && exit 1
+
+elif [ ! -z "${gds_file}" ]
+then
+    "${R}" --slave --vanilla --file="${SCRIPTS_DIR}/generate_snp_sequence.R" --args -d "${mnn_filtered_file}" -l "${ld_threshold}" -m "${maf_threshold}" -M "${missing_rate}" -o "${prefix_output}" > /dev/null
+    [ $? != 0 ] && exit 1
+
+fi
+
+#"${R}" --slave --vanilla --file="${SCRIPTS_DIR}/generate_snp_sequence.R" --args -H "${mnn_filtered_file}" -l "${ld_threshold}" -m "${maf_threshold}" -M "${missing_rate}" -o "${prefix_output}" #hapmap
+#"${R}" --slave --vanilla --file="${SCRIPTS_DIR}/generate_snp_sequence.R" --args -v "${mnn_filtered_file}" -l "${ld_threshold}" -m "${maf_threshold}" -M "${missing_rate}" -o "${prefix_output}" #VCF
+#[ $? != 0 ] && exit 1
+
+
 #output .fasta, and .picked.hapmap
 picked_file="${prefix_output}.picked"
 
@@ -346,7 +375,7 @@ echo "Finally picked; ${seq_len} SNPs"
 
 
 #SNP size restriction -pseudo function
-if [ ${ignore_kill_code} != 1 ]; then
+if [ ${ignore_kill_code} -ne 1 ]; then
 
     #additional options for sequence length check
     if [ ${seq_len} -gt 20000 ] || [ ${seq_len} -lt 5000 ]; then  #[ ${ld_threshold} -ne 0 ]; then # && [ ${ld_threshold} -ne 0 ]; then
@@ -367,56 +396,57 @@ fi
 if [[ ${admix_from} -ge 1 && ${admix_to} -ge ${admix_from} ]]
 then
 
-	admixture_folder="${prefix_output_folder}/admixture"
+    admixture_folder="${prefix_output_folder}/admixture"
 
-	if [ ! -e ${admixture_folder} ]
-	then
-		mkdir ${admixture_folder}
-	else
-		rm -f ${admixture_folder}/*
-	fi
+    if [ ! -e ${admixture_folder} ]
+    then
+        mkdir ${admixture_folder}
+    else
+        rm -f ${admixture_folder}/*.*
+    fi
 
-	#move to working folder
-	cd ${admixture_folder}
-	admixture_prefix="${admixture_folder}/out"
+    #move to working folder
+    cd ${admixture_folder}
+    admixture_prefix="${admixture_folder}/out"
 
-	echo
-	echo "--admixture start"
-	echo "Prepare Admixture..."
-	#convert ACGT format -> 1,2 format by plink
-	${PLINK} --noweb --file "${picked_file}" --recode12 --out "${admixture_prefix}_12" > /dev/null #log file is not necessary
-	[ $? != 0 ] && exit 1
-	echo "Obtain; ${admixture_prefix}_12.ped(map), --recode12" 
+    echo
+    echo "--Admixture start"
+    echo "Prepare Admixture..."
+    #convert ACGT format -> 1,2 format by plink
+    ${PLINK} --noweb --file "${picked_file}" --recode12 --out "${admixture_prefix}_12" > /dev/null #log file is not necessary
+    [ $? != 0 ] && exit 1
+    echo "Obtain; ${admixture_prefix}_12.ped(map), --recode12" 
 
-	echo
-	echo "Admixture analysis proceed..."
-	#population structure analysis using "Admixture", 2014.5.16
-	echo "cv_error_trend" > "${admixture_prefix}.cv_error"
+    echo
+    echo "Admixture analysis proceed..."
+    #population structure analysis using "Admixture", 2014.5.16
+    echo "cv_error_trend" > "${admixture_prefix}.cv_error"
 
-	for ((k=2;k<8;k++)) #2 to 7
-		do
-		    #"${SCRIPTS_DIR}/admixture" --cv "${prefix_output}_12" $k, admixture require 1,2 based ped
-		    ${ADMIXTURE} --cv "${admixture_prefix}_12.ped" ${k} > "${admixture_prefix}_12.${k}.log"
-		    [ $? != 0 ] && exit 1
+    #for k in {${admix_from}..${admix_to}}
+    for ((k=${admix_from}; k<=${admix_to}; k++)) #2 to 7
+    do
+        #"${SCRIPTS_DIR}/admixture" --cv "${prefix_output}_12" $k, admixture require 1,2 based ped
+        ${ADMIXTURE} --cv "${admixture_prefix}_12.ped" ${k} > "${admixture_prefix}_12.${k}.log"; [ $? != 0 ] && exit 1
 
-		    echo -e "${k}\t$( grep "CV" "${admixture_prefix}_12.${k}.log")" >> "${admixture_prefix}.cv_error"
-		    echo "1- tree k=${k}" 
+        echo -e "${k}\t$( grep "CV" "${admixture_prefix}_12.${k}.log")" >> "${admixture_prefix}.cv_error"
+        echo "1- tree k=${k}" 
 
-		    rm -f "${admixture_prefix}_12.${k}.log"
+        rm -f "${admixture_prefix}_12.${k}.log"
 
-		    #draw barplot
-		    "${R}" --slave --vanilla --file="${SCRIPTS_DIR}/barplot_multi.R" --args "${admixture_prefix}_12.${k}.Q"
-		    echo "2- obtain figure ${admixture_prefix}_12.${k}.Q.png"
-		    [ $? != 0 ] && exit 1
+        #draw barplot
+        #"${R}" --slave --vanilla --file="${SCRIPTS_DIR}/barplot_multi.R" --args "${admixture_prefix}_12.${k}.Q"
+        "${R}" --slave --vanilla --file="${SCRIPTS_DIR}/barplot_multi_index.R" --args -n "${admixture_prefix}_12.nosex" "${admixture_prefix}_12.${k}.Q" #sorted barplot
+        echo "2- obtain figure ${admixture_prefix}_12.${k}.Q.png"
+        [ $? != 0 ] && exit 1
 
-		done
+    done
 
 
-	cd .. #admixture out
-	echo "--admixture done"
+    cd .. #admixture folder out
+    echo "--Admixture done"
 
 else
-	echo "Admixture analysis, skip"
+    echo "--Admixture skip, -K and -k = 0"
 
 fi
 
@@ -429,11 +459,11 @@ then
     #[ ! -e "${vcf_file}" ] && print_help_and_exit 1 "treemix_index_file (${treemix_index_file}) was not found!"
     #convert hapmap to treemix input format
 
-	echo
-	echo "TreeMix analysis proceed..."
+    echo
+    echo "TreeMix analysis proceed..."
 
-	#check if specified root exists in index file
-	grep ${treemix_root} ${treemix_index_path}
+    #check if specified root exists in index file
+    grep ${treemix_root} ${treemix_index_path}
     [ $? != 0 ] && print_help_and_exit 1 "Root ${treemix_root} was not found in (${treemix_index_path})"
 
     treemix_folder="${prefix_output_folder}/treemix"
@@ -442,7 +472,7 @@ then
     then
         mkdir ${treemix_folder}
     else
-        rm -f ${treemix_folder}/*
+        rm -f ${treemix_folder}/*.*
     fi
 
     #move to working folder
@@ -451,7 +481,7 @@ then
 
     #convert pciked.ped to hapmap
     echo
-    echo "--treemix start"
+    echo "--Treemix start"
     echo "Prepare TreeMix..."
     #echo "Convert ${prefix_output}.picked.ped(.map) -> ${prefix_output}.picked.hapmap"
     #1. ped -> tped
@@ -489,7 +519,7 @@ then
     echo "4- obtain figure ${treemix_prefix}.pnd"
 
     cd .. #treemix out
-    echo "--treemix done"
+    echo "--Treemix done"
 
 else
     echo "Proceed without TreeMix analysis(no -t specified or ${tree_index_path} not exists}"
@@ -497,79 +527,82 @@ else
 fi
 
 
-if [ ${num_bs_sample} -ge 100 ] #since Bs minimum is 100
+if [ ${num_bs_sample} -ge 100 ] #since Bootstrap minimum is 100
 then
 
-	snphylo_folder="${prefix_output_folder}/snphylo"
+    snphylo_folder="${prefix_output_folder}/snphylo"
 
-	if [ ! -e ${snphylo_folder} ]
-	then
-	    mkdir ${snphylo_folder}
-	else
-	    rm -f ${snphylo_folder}/*
-	fi
+    if [ ! -e ${snphylo_folder} ]
+    then
+        mkdir ${snphylo_folder}
+    else
+        rm -f ${snphylo_folder}/*.*
+    fi
 
-	#move to working folder
-	cd ${snphylo_folder}
-	snphylo_prefix="${snphylo_folder}/out"
+    #move to working folder
+    cd ${snphylo_folder}
+    snphylo_prefix="${snphylo_folder}/out"
 
-	echo
-	echo "--snphylo start"
-	echo "MSA proceed using ${seq_len} SNPs"
+    echo
+    echo "--SNPhylo start"
+    echo "MSA proceed using ${seq_len} SNPs"
 
-	#SNP sequence alignment using MUSCLE, takes much of time, should consider accuracy depend on argument usage
-	if [ ${seq_len} -lt 50000 ]; then #normal alignment
-	    "${MUSCLE}" -phyi -in ${prefix_output}.fasta -out ${snphylo_prefix}.phylip.txt; [ $? != 0 ] && exit 1
+    #SNP sequence alignment using MUSCLE, takes much of time, should consider accuracy depend on argument usage
+        
+    #using clustalo (clustal omega) that supports multithreading for large datasets
+    if [ ${seq_len} -ne 0 ]; then #clustal omega alignment, output phylip format --outfmt=phy
+        echo "clustalo --threads=${n_thread} -i ${prefix_output}.fasta -o ${snphylo_prefix}.align.txt --outfmt=fasta"
+        clustalo --threads=${n_thread} -i ${prefix_output}.fasta -o ${snphylo_prefix}.align.txt --outfmt=fasta; [ $? != 0 ] && exit 1
 
-	elif [ ${seq_len} -lt 100000 ]; then #large
-	    "${MUSCLE}" -phyi -in ${prefix_output}.fasta -out ${snphylo_prefix}.phylip.txt -maxiters 2; [ $? != 0 ] && exit 1
-	    
-	elif [ ${seq_len} -ge 100000 ]; then #huge, -ge greater or equal
-	    "${MUSCLE}" -phyi -in ${prefix_output}.fasta -out ${snphylo_prefix}.phylip.txt -maxiters 1 -diags -sv; [ $? != 0 ] && exit 1
+    else
+        echo "Zero length to align using clustalo. Exit"
+	exit 1
 
-	fi
-	#-maxiters, default=16
-	#-diags, dynamic programming rely on k-mer clustering, faster process, but lower accuracy
-
-
-	echo
-	echo "BS tree draw proceed"
-
-	## Determine a phylogenetic tree(maximum likelihood)
-	for rm_file in outfile outtree
-	do
-	    [ -e "${rm_file}" ] && rm -f "${rm_file}"
-	done
-
-	ln -sf ${snphylo_prefix}.phylip.txt infile
-
-	if [ -z "${out_sample_id}" ]
-	then
-	    echo -e "y\n" | "${DNAML}"; [ $? != 0 ] && exit 1
-	else
-		out_sample_no=$[$(grep -ne "\<${out_sample_id}\>" infile | cut -f1 -d":") - 1]
-	    if [ ${out_sample_no} -eq -1 ]
-	    then
-	    echo "Error!!! There is no sample name (${out_sample_id}) to use as a outgroup in a tree input file (${snphylo_prefix}.phylip.txt)." 1>&2
-	    echo "Please check the name and restart this script." 1>&2
-	    rm -f infile
-	    exit 1
-	    else
-	    echo -e "o\n${out_sample_no}\ny\n" | "${DNAML}"; [ $? != 0 ] && exit 1
-	    fi
-	fi
-
-	mv outfile "${snphylo_prefix}.ml.txt"
-	mv outtree "${snphylo_prefix}.ml.tree"
-
-	rm -f infile
-
-    "${R}" --slave --vanilla --file="${SCRIPTS_DIR}/determine_bs_tree.R" --args -i "${snphylo_prefix}.ml.tree" -o "${snphylo_prefix}" -p "${snphylo_prefix}.phylip.txt" -n ${num_bs_sample} > /dev/null
-    [ $? != 0 ] && exit 1
+    fi
+    #-maxiters, default=16
+    #-diags, dynamic programming rely on k-mer clustering, faster process, but lower accuracy
 
 
-	cd .. #snphylo out
-	echo "--snphylo done"
+    echo
+    echo "Bootstrap tree draw proceed"
+
+    ## Determine a phylogenetic tree(maximum likelihood)
+    for rm_file in outfile outtree
+    do
+        [ -e "${rm_file}" ] && rm -f "${rm_file}"
+    done
+
+    #ln -sf ${snphylo_prefix}.phylip.txt infile #symbolic link
+
+    if [ -z "${out_sample_id}" ]
+    then
+        #echo "raxmlHPC-PTHREADS-AVX -T 6 -s ./infile -n ./outfile -m GTRCAT -f a" #print what is using
+        #raxmlHPC-PTHREADS-AVX -T 6 -s ./infile -n ./outfile -m GTRCAT -f a; [ $? != 0 ] && exit 1 #using raxMLHPC    
+        echo "raxml-ng --threads ${n_thread} --msa ${snphylo_prefix}.align.txt  --msa-format FASTA --prefix ${snphylo_prefix} --model GTR+G --all --bs-trees ${num_bs_sample}"
+        raxml-ng --threads ${n_thread} --msa ${snphylo_prefix}.align.txt --msa-format FASTA --prefix ${snphylo_prefix} --model GTR+G --all --bs-trees ${num_bs_sample} > /dev/null; [ $? != 0 ] && exit 1
+
+        echo "# run-log saved in ${snphylo_prefix}.raxml.log"
+
+    else
+        out_sample_no=$[$(grep -ne "\<${out_sample_id}\>" infile | cut -f1 -d":") - 1]
+        
+            if [ ${out_sample_no} -eq -1 ]
+        then
+            echo "Error!!! There is no sample name (${out_sample_id}) to use as a outgroup in a tree input file (${snphylo_prefix}.align.txt)." 1>&2
+            echo "Please check the name and restart this script." 1>&2
+            rm -f infile
+            exit 1
+        else
+            echo -e "o\n${out_sample_no}\ny\n" | "${DNAML}"; [ $? != 0 ] && exit 1
+        fi
+    fi
+
+    #mv outfile "${snphylo_prefix}.ml.txt"
+    #mv outtree "${snphylo_prefix}.ml.tree"
+    #rm -f infile #remove symbolic link
+
+    cd .. #snphylo out
+    echo "--SNPhylo done"
 
 fi
 
